68b62701bbf1101a2b70b808ceefdee9e15a9ed3
rickblankestijn <rickblankestijn.blankestijn@student.hu.nl>
2017-09-12 15:00:53 +0200

    huiswerk

--- 2.1.py
+++
@@ -1,5 +0,0 @@
-letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
-
-voorkomen = [letters.count('A'), letters.count('B'), letters.count('C')]
-
-print(voorkomen)
--- 2.4.py
+++
@@ -1,3 +0,0 @@
-salaris = int(input('wat verdien je per uur'))
-uren = int(input('hoeveel uur heb je gewerkt deze week'))
-print(str(uren) + 'uur werken levert ' + str(salaris * uren) + 'euro op')
