926eaa34c1a9461b07249fa8cfac793fef9953bf
rickblankestijn <rickblankestijn.blankestijn@student.hu.nl>
2017-09-12 14:58:57 +0200

    huiswerk

---
+++ Les 3/3.1.py
@@ -0,0 +1,7 @@
+score = (input('Hoeveel punten heb je gehaald?'))
+if int(score) >=15:
+    print('gefeliciteerd! met ' + score + ' punten ben je geslaagd.')
+elif int(score) <15:
+    print('sorry, met ' + score + ' punten ben je niet geslaagd.')
+
+
