7837bdcea26cb4d799325661eabadad8d8c8bf26
rickblankestijn <rickblankestijn.blankestijn@student.hu.nl>
2017-09-12 14:54:52 +0200

    Initial commit

--- 3.1.py
+++ 3.1.py
@@ -1,1 +1,5 @@
-print('.')
+score = (input('Hoeveel punten heb je gehaald?'))
+if int(score) >=15:
+    print('gefeliciteerd! met ' + score + ' ' + 'punten ben je geslaagd.')
+if int(score) <15:
+    print('sorry, met ' + score + ' ' + 'punten ben je niet geslaagd.')
